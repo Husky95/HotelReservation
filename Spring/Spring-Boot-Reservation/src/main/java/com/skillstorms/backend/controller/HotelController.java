ea56d514524af3425ae16d25eb52d4009db12dd4
Gregory Mannerberg <gmannerberg@gmail.com>
2022-07-27 20:28:31 -0400

    Hotel PUT

--- Spring/Spring-Boot-Reservation/src/main/java/com/skillstorms/backend/controller/HotelController.java
+++ Spring/Spring-Boot-Reservation/src/main/java/com/skillstorms/backend/controller/HotelController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -49,5 +50,15 @@ public class HotelController {
   @GetMapping(path="/name/{name}")
   public @ResponseBody List<Hotel> findByName(@PathVariable String name) {
   	return hotelRepository.findByHotelName(name);
+  }
+  @PutMapping(path="")
+  public @ResponseBody Hotel updateHotel(@Valid @RequestBody Hotel hotel) {
+  	Optional<Hotel> hotelDatabaseVersion = hotelRepository.findById(hotel.getHotelID());
+  	if (hotelDatabaseVersion.isPresent()) {
+  		return hotelRepository.save(hotel);
+  	}
+  	else {
+  		throw new ResourceNotFoundException();
+  	}
   }
 }
