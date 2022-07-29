28c5f9421f213dae1c8a7d515e3ed647517dcedb
Husky95 <kurst811@gmail.com>
2022-07-29 13:46:35 -0700

    fix duplicate primary key in reservation

--- Spring/Spring-Boot-Reservation/src/main/java/com/skillstorms/backend/model/Reservation.java
+++ Spring/Spring-Boot-Reservation/src/main/java/com/skillstorms/backend/model/Reservation.java
@@ -22,7 +22,7 @@ import com.fasterxml.jackson.annotation.JsonIdentityReference;
 @Table(name="Reservation")
 public class Reservation {
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
     @Column(name = "Reservation_Number")
 	int reservationNumber;
 	
